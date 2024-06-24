ca2fdb7150fc2e244f6dc3ee75da372dbce26eda
Daniele <dsmarinho@gmail.com>
2024-06-24 16:06:40 -0300

    Adding tests Update and Delete & Updating create endpoint

--- tests.py
+++ tests.py
@@ -14,7 +14,7 @@ def test_create_task():
         "description": "Descrição da nova tarefa"
     }
 
-    #vai enviar a requisição, json vai enviar o dado que eu quero - o post responde um objeto.
+    #vai enviar a requisição, json vai enviar o dado que eu quero; - O post responde um objeto.
     response = requests.post(f"{BASE_URL}/tasks", json=new_task_data) #
 
     #assert - verificar / validar o status. se o status code for 200 deu tudo certo neste caso
@@ -45,7 +45,8 @@ def test_get_tasks():
 
 #Recuperar uma tarefa específica
 def test_get_task():
-    #usar a lista task criada anteriormente
+
+    #usar a lista tasks criada anteriormente
     if tasks:
         task_id  = tasks[0] #pegar a 1ª atividade criada, 1º posicao do id     
         response = requests.get(f"{BASE_URL}/tasks/{task_id}")  #enviar a url com o id na requisição GET
@@ -55,4 +56,59 @@ def test_get_task():
         response_json = response.json() #este método vai trazer o corpo da resposta
         
         #comparar se o task_id é igual ao identificador que estou tesntando recuperar (response_json['id'])
-        assert task_id == response_json["id"]
+        assert task_id == response_json["id"]  #fazer as condições/recuperações - status_code e id
+
+#UPDATE
+def test_update_task():
+    #usar a lista tasks criada anteriormente, se tiver algo na lista pegar a 1ª posição criada - task[0]
+    if tasks:
+        task_id = tasks[0]
+
+        #payload vai ter os dados a serem atualizados
+        payload = {
+            "title": "Título atualizado",
+            "description": "Nova descrição",
+            "completed": True
+        }
+
+        response = requests.put(f"{BASE_URL}/tasks/{task_id}", json=payload)  #enviar a url com o id na requisição GET e o jon com o corpo;  #vai enviar a requisição, json vai enviar o dado que eu quero; #enviar a requisição usando o PUT e enviar o payload(dicionario criado com todas as informações) no json 
+
+        #fazer as comparações - status_code
+        assert response.status_code == 200
+
+        #recuperar o json na response e fazer as demais validações; #este método vai trazer o corpo da resposta
+        response_json = response.json()
+
+        #validar a messagem
+        assert "message" in response_json
+
+        #Automatizar  a verificação - Nova requisição a tarefa especifica
+        # repetir o get_task e alterar as comparações; - vai atualizar e fazer outra requisição
+
+        #enviar a url com o id na requisição GET
+        response = requests.get(f"{BASE_URL}/tasks/{task_id}")
+
+        #fazer as comparações - status_code 
+        assert response.status_code == 200
+        
+        #chamar o json() - este método vai trazer o corpo da resposta
+        response_json = response.json()
+        #fazer as comparações - status_code, title, description, completed com o payload
+        assert response_json["title"]       == payload["title"]
+        assert response_json["description"] == payload["description"]
+        assert response_json["completed"]   == payload["completed"]
+
+
+#DELETE
+def test_delete_task():
+    #usar a lista tasks criada anteriormente, se tiver algo na lista pegar a 1ª posição criada - task[0]
+    if tasks:
+        task_id = tasks[0]
+
+        response = requests.delete(f"{BASE_URL}/tasks/{task_id}") #usar o método delete nas requisições      
+        assert response.status_code == 200 #validar o status_code == 200
+
+        #usar a tarefa especifica (GET) para finalizar o teste. A msg deve ser 404, ou seja ela nao pode ser encontrada pois foi excluida (status_code == 404)
+      
+        response = requests.get(f"{BASE_URL}/tasks/{task_id}")
+        assert response.status_code == 404     
